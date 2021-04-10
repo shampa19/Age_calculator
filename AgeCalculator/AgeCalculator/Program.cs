87df0d23a17f4fcd5837ca8142b811650f6cc9d5
shampa19 <76871452+shampa19@users.noreply.github.com>
2021-04-11 00:53:47 +0600

    Add files via upload

---
+++ AgeCalculator/AgeCalculator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace AgeCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             Console.WriteLine("Enter your Date of Birth:");
+             DateTime dateOfBith = Convert.ToDateTime( Console.ReadLine());
+
+             DateTime date1 = DateTime.Now;
+
+            TimeSpan diff1 = date1 - dateOfBith;
+            DateTime Age = DateTime.MinValue + diff1;
+          
+
+            int Years = Age.Year - 1;
+            int Months = Age.Month - 1;
+            int Days = Age.Day - 1;
+            Console.Write(Days.ToString() + " Days " + Months.ToString() + " Months, " + Years.ToString() + " Years, " );
+
+        }
+    }
+}
